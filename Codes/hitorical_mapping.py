312c3eb1af3b841258d8c76e0483caaa460234d2
himanchal-medz <ds.two@medz91.com>
2023-03-01 02:47:19 +0530

    Added Historical logic

--- Codes/hitorical_mapping.py
+++ Codes/hitorical_mapping.py
@@ -1,5 +1,27 @@
-import csv
-import pandas as pd
+import random
+import subprocess
+import matplotlib.pyplot as plt
 import numpy as np
+import os
+import pandas as pd
+import re
+import requests
+import time
+import numpy as np
+import os
+import pandas as pd
+import datetime
+import calendar
+import time
+import warnings
+import datetime as dt
+import time
+import gc
+from collections import OrderedDict
+import math
+from collections import Counter
 
-print(np.__version__)
+
+historical_map = pd.read_csv('../historical_mapping/historical_map.csv')
+historical_map = historical_map[['item_code','brand_x', 'brand_y', 'id']]
+distributor_data = pd.read_csv('../historical_mapping/historical_map.csv')
